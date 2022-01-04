ea5d162cc717b4c49eac652bae09dc1f4438fa6a
Giabao <luugiabaoluu24@gmail.com>
2022-01-04 17:00:04 +0700

    decorated

--- src/styles.js
+++ src/styles.js
@@ -1,16 +1,29 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
+  [theme.breakpoints.down('sm')]: {
+    mainContainer: {
+      flexDirection: 'column-reverse',
+    },
+  },
   appBar: {
     backgroundColor: 'rgb(255, 200, 48)',
     borderRadius: 15,
     margin: '30px 0',
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'left',
     alignItems: 'center',
   },
+
+  heading: {
+    marginLeft: '20px',
+    marginTop: '13px',
+    fontSize: '50px'
+  },
+
   image: {
     marginLeft: '15px',
   },
 }));
+
